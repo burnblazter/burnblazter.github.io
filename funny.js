3fb8baaf3e33261856dec1f132e8ec7d6cef3b2e
Fael <113332224+burnblazter@users.noreply.github.com>
2023-02-15 17:26:24 +0800

    fix js js

--- funny.js
+++ funny.js
@@ -1,10 +1,9 @@
-setTimeout(function() {
+setTimeoutsetTimeout(function() {
   const urls = [
     'https://youtu.be/0hilvfWb4Bo',
     'https://youtu.be/D9rG-qMahpE',
     'https://youtu.be/4ricQRaEmP4'
-    'https://youtu.be/OSgRqcvfJjw'
-    
+ 
   ];
 
   const randomUrl = urls[Math.floor(Math.random() * urls.length)];
